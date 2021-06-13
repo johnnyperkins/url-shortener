e147d5d6db17e04f98f39d6fe83789bf79122e87
jump <jmperkinsbiz@gmail.com>
2021-06-13 14:12:02 -0700

    ui:css: use border-box sizing model

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -3,7 +3,19 @@
 </template>
 
 <style lang="scss">
+html {
+  box-sizing: border-box;
+}
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
 #app {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-items: center;
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
