860d4a9f0d5e8bcd58c9164e791bc8346d8cd0d8
jump <jmperkinsbiz@gmail.com>
2021-06-13 16:23:14 -0700

    ui:Home: use components ShortUrlForm & ShortUrlResult + styling

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,11 +1,55 @@
 <template>
   <div class="home">
     <h1>URL Shortener</h1>
+
+    <short-url-form @success="onSubmitSucces" />
+
+    <short-url-result
+      v-for="url in generatedUrls"
+      :key="url.shortUrl"
+      class="result"
+      :longUrl="url.longUrl"
+      :shortUrl="url.shortUrl"
+    />
   </div>
 </template>
 
 <script>
+import ShortUrlForm from '@/components/ShortUrlForm.vue'
+import ShortUrlResult from '@/components/ShortUrlResult.vue'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+
+  data () {
+    return {
+      generatedUrls: []
+    }
+  },
+
+  methods: {
+    onSubmitSucces (generatedUrlResponse) {
+      this.generatedUrls.push(generatedUrlResponse)
+    }
+  },
+
+  components: {
+    ShortUrlForm,
+    ShortUrlResult
+  }
 }
 </script>
+
+<style scoped lang="scss">
+@import '@/css/sizes.scss';
+
+.home {
+  width: 100%;
+  max-width: 700px;
+
+  .result {
+    width: 100%;
+    margin-top: $space-lg;
+  }
+}
+</style>
