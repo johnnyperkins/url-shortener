498029c097a48d7e165cebbbad94a67e89c5c523
jump <jmperkinsbiz@gmail.com>
2021-06-13 15:55:39 -0700

    ui: create component ShortUrlForm + tests

---
+++ ui/src/components/ShortUrlForm.unit.js
@@ -0,0 +1,120 @@
+import ShortUrlForm from './ShortUrlForm.vue'
+import { mount } from '@vue/test-utils'
+import * as api from '@/services/api'
+
+describe('ShortUrlForm.vue', () => {
+  it('triggers form submit on button click', async () => {
+    const wrapper = mount(ShortUrlForm)
+    const vm = wrapper.vm
+    const submitSpy = jest.spyOn(vm, 'submit')
+    const submitBtn = wrapper.get('[data-test="submitBtn"]')
+    await submitBtn.trigger('submit')
+    expect(submitSpy).toBeCalled()
+  })
+
+  it('correctly calls create url api on submit', async () => {
+    const urlInputValue = 'http://www.testurl.com/some-long-url/mock'
+    const createShortUrlSpy = jest.spyOn(api, 'createShortUrl')
+
+    const wrapper = mount(ShortUrlForm)
+    const urlInput = wrapper.get('[data-test="urlInput"]')
+    const submitBtn = wrapper.get('[data-test="submitBtn"]')
+
+    await urlInput.setValue(urlInputValue)
+    await submitBtn.trigger('submit')
+
+    expect(createShortUrlSpy).toHaveBeenCalledWith(urlInputValue)
+  })
+
+  it('emits correct value on submit success', async () => {
+    const urlInputValue = 'http://www.testurl.com/some-long-url/mock'
+    const mockCreateShortUrlResponse = 'http://urlshortener.com/1'
+
+    jest.spyOn(api, 'createShortUrl')
+      .mockImplementation(() => mockCreateShortUrlResponse)
+
+    const wrapper = mount(ShortUrlForm)
+    const urlInput = wrapper.get('[data-test="urlInput"]')
+    const submitBtn = wrapper.get('[data-test="submitBtn"]')
+
+    await urlInput.setValue(urlInputValue)
+    await submitBtn.trigger('submit')
+
+    expect(wrapper.emitted().success[0][0])
+      .toStrictEqual({
+        shortUrl: mockCreateShortUrlResponse,
+        longUrl: urlInputValue
+      })
+  })
+
+  it('emits error on submit failure', async () => {
+    const mockError = new Error('fail')
+
+    jest.spyOn(api, 'createShortUrl')
+      .mockRejectedValue(mockError)
+
+    const wrapper = mount(ShortUrlForm)
+    await wrapper.vm.submit()
+
+    expect(wrapper.emitted().fail[0][0])
+      .toBe(mockError)
+  })
+
+  it('displays error message on submit failure', async () => {
+    const mockError = new Error('fail')
+
+    jest.spyOn(api, 'createShortUrl')
+      .mockRejectedValue(mockError)
+
+    const wrapper = mount(ShortUrlForm)
+    await wrapper.vm.submit()
+
+    expect(wrapper.text()).toContain(mockError.message)
+  })
+
+  it('disables submit button while loading', async () => {
+    const wrapper = mount(ShortUrlForm, {
+      data() {
+        return {
+          isLoading: true
+        }
+      }
+    })
+
+    const submitBtn = wrapper.get('[data-test="submitBtn"]')
+    const disabledAttribute = { disabled: '' }
+    expect(submitBtn.attributes()).toMatchObject(disabledAttribute)
+  })
+
+  it('clears url input on submit success', async () => {
+    const urlInputValue = 'http://www.testurl.com/some-long-url/mock'
+
+    jest.spyOn(api, 'createShortUrl')
+      .mockImplementation(() => true)
+
+    const wrapper = mount(ShortUrlForm)
+    const urlInput = wrapper.get('[data-test="urlInput"]')
+    const submitBtn = wrapper.get('[data-test="submitBtn"]')
+
+    await urlInput.setValue(urlInputValue)
+    await submitBtn.trigger('submit')
+
+    expect(urlInput.element.value).toBe('')
+  })
+
+  it('does not clear url input on submit failure', async () => {
+    const urlInputValue = 'http://www.testurl.com/some-long-url/mock'
+
+    jest.spyOn(api, 'createShortUrl')
+      .mockRejectedValue(new Error('x'))
+
+    const wrapper = mount(ShortUrlForm)
+    const urlInput = wrapper.get('[data-test="urlInput"]')
+    const submitBtn = wrapper.get('[data-test="submitBtn"]')
+
+    await urlInput.setValue(urlInputValue)
+    await submitBtn.trigger('submit')
+
+    expect(urlInput.element.value).toBe(urlInputValue)
+  })
+})
---
+++ ui/src/components/ShortUrlForm.vue
@@ -0,0 +1,85 @@
+<template>
+  <form class="form" @submit.prevent="submit">
+    <p v-if="error" class="error">{{ error }}</p>
+
+    <div class="inputWrap">
+      <input
+        v-model="url"
+        data-test="urlInput"
+        type="text"
+        placeholder="Enter URL"
+      >
+
+      <button
+        type="submit"
+        data-test="submitBtn"
+        :disabled="isLoading"
+      >
+        Shorten
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+import { createShortUrl } from '@/services'
+
+export default {
+  name: 'ShortUrlForm',
+
+  data () {
+    return {
+      url: '',
+      isLoading: false,
+      error: null
+    }
+  },
+
+  methods: {
+    async submit () {
+      this.isLoading = true
+      this.error = null
+
+      try {
+        const shortUrl = await createShortUrl(this.url)
+
+        this.$emit('success', {
+          shortUrl,
+          longUrl: this.url
+        })
+
+        this.url = ''
+      } catch (error) {
+        this.error = error.message
+        this.$emit('fail', error)
+        // log error
+      } finally {
+        this.isLoading = false
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.form {
+  width: 100%;
+
+  .inputWrap {
+    display: flex;
+    flex-wrap: nowrap;
+
+    > input {
+      width: 100%;
+    }
+
+    > button {
+      margin-left: 8px;
+    }
+  }
+
+  .error {
+    color: red;
+  }
+}
+</style>
