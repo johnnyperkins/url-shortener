a753d76603a96b31dcd7a0892b78d85575e85682
jump <jmperkinsbiz@gmail.com>
2021-06-13 18:48:47 -0700

    server:npm: add package redis

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const cors = require('cors')
+const redis = require('redis')
 const app = express()
 const port = 3000
+const redisClient = redis.createClient()
 
 app.use(cors())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+redisClient.on('error', function(error) {
+  console.error(error)
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
