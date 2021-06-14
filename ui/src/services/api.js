671ce69327ac5f8fd57350a7c6fd405596ae587e
jump <jmperkinsbiz@gmail.com>
2021-06-13 17:14:54 -0700

    ui:api: add method getOriginalUrl

--- ui/src/services/api.js
+++ ui/src/services/api.js
@@ -2,8 +2,18 @@
  * Creates a short url
  *
  * @param {string} url
- * @returns {Promise}
+ * @returns {Promise<String>}
  */
 export const createShortUrl = async (url = '') => {
   return `http://urlshortener.com/${Math.random()}`
+}
+
+/**
+ * Returns the original url from a short url
+ *
+ * @param {string} shortUrl
+ * @returns {Promise<String>}
+ */
+export const getOriginalUrl = async (shortUrl = '') => {
+  return 'http://digitalgrandeur.com'
 }
